8d8e4bb1612519fec246a5c96d3b0c24aa8a4787
HennieScheepers <58654609+HennieScheepers@users.noreply.github.com>
2022-05-03 17:20:27 +0200

    fixed the playRound function when user input is scissor. Added game function

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,4 @@
-//get the users choice between rock, paper, scissors
-let input = prompt('Choose your weapon: (rock, paper, scissors)');
-let firstLetter = input.slice(0,1);
-let restOfWord = input. slice(1)
-let playerSelection = firstLetter.toUpperCase()+restOfWord.toLowerCase();
+
 
 function computerPlay() {
     let choiceArray = ['Rock','Paper','Scissor'];
@@ -13,24 +9,43 @@ function computerPlay() {
 function playRound(cpuChoice,playerChoice) {
     if (cpuChoice=='Rock' && playerChoice=='Paper') {
         let outcome = 'Player wins! Paper beats rock';   
-    } else if(cpuChoice=='Rock' && playerChoice=='Paper'){
+        return outcome;
+    } else if(cpuChoice=='Rock' && playerChoice=='Scissor'){
         let outcome = 'Cpu wins! ' + cpuChoice + ' beats ' + playerChoice;
         return outcome;
     } 
-    if (cpuChoice=='Paper' && playerChoice=='Scissors') {
+
+    if (cpuChoice=='Paper' && playerChoice=='Scissor') {
         let outcome = 'Player wins! Paper beats rock';  
         return outcome;
     } else if(cpuChoice=='Paper'&& playerChoice=='Rock'){
         let outcome = 'Cpu wins! ' + cpuChoice + ' beats ' + playerChoice;
         return outcome;
     }
-    if (cpuChoice=='Scissors' && playerChoice=='Rock') {
+
+    if (cpuChoice=='Scissor' && playerChoice=='Rock') {
         let outcome = 'Player wins! Paper beats rock';  
         return outcome;
-    } else if(cpuChoice=='Scissors' && playerChoice=='Paper'){
+    } else if(cpuChoice=='Scissor' && playerChoice=='Paper'){
         let outcome = 'Cpu wins! ' + cpuChoice + ' beats ' + playerChoice;
         return outcome;
     }
-};
 
-console.log(playRound(computerPlay(),playerSelection));
+    if (playerChoice===cpuChoice) {
+        let outcome = "It's a draw!";
+        return outcome;
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let input = prompt('Choose your weapon: (rock, paper, scissor)');
+        let firstLetter = input.slice(0,1);
+        let restOfWord = input. slice(1)
+        let playerSelection = firstLetter.toUpperCase()+restOfWord.toLowerCase();
+
+        console.log('Round ' + (i+1).toString() + ':' + playRound(computerPlay(),playerSelection));
+    }
+}
+
+game();
